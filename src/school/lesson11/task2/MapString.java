fa4254f107ace677481090cffcc57e6f90c5447f
Witold101 <vi_stal@hotmail.com>
2018-05-30 13:03:16 +0300

    Sinchronisation

--- src/school/lesson11/task2/MapString.java
+++ src/school/lesson11/task2/MapString.java
@@ -1,23 +1,26 @@
 package school.lesson11.task2;
 
-import java.util.Map;
+import java.util.*;
 
 /**
  * Написать метод isUnique, который принимает Map<String, String> и возвращает true,
  * если два различных ключа не соответствуют двум одинаковым значениям.
  * Например, в данном случае вернётся true:
- *
+ * <p>
  * {Marty=Stepp, Stuart=Reges, Jessica=Miller, Amanda=Camp, Hal=Perkins}
- *
+ * <p>
  * Авданном false:
- *
+ * <p>
  * {Kendrick=Perkins, Stuart=Reges, Jessica=Miller, Bruce=Reges, Hal=Perkins}
  */
 
 public class MapString {
 
-    public static boolean isUnique(Map<String,String> stringMap){
-        
-        return true;
+    public static boolean isUnique(Map<String, String> stringMap) {
+        Set<String> stringSet =new HashSet<>(stringMap.values());
+        return stringMap.keySet().size()==stringSet.size();
     }
+
+    Map<String,String> map = new HashMap<>();
+    
 }
